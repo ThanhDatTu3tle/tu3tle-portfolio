7c401c90c1cc0c8265361188104ed630d7fa9e98
ThanhDatTu3tle <71086168+ThanhDatTu3tle@users.noreply.github.com>
2023-02-20 22:15:46 +0700

    69% loading...

--- src/layouts/components/Header/Header.tsx
+++ src/layouts/components/Header/Header.tsx
@@ -2,9 +2,9 @@ import * as React from 'react';
 // import { useState, useEffect } from 'react';
 // import { Link } from 'react-router-dom';
 import classNames from "classnames/bind";
-// import FacebookIcon from '@mui/icons-material/Facebook';
 
 import styles from './Header.module.scss';
+import Dropdown from '../../../components/Dropdown/Dropdown';
 
 const cx = classNames.bind(styles)
 
@@ -16,11 +16,7 @@ const Header: React.FC<any> = () => {
             </div>
 
             <div className={cx('shortcut')}>
-                <h4>Work</h4>
-                <h4>About</h4>
-                <h4>Blog</h4>
-                <h4>Resume</h4>
-                <h4>Contact</h4>
+                <Dropdown />
             </div>
         </div>
     )
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -5,6 +5,8 @@ import classNames from "classnames/bind";
 
 import styles from './Home.module.scss';
 import Header from '../../layouts/components/Header/Header';
+import Title from '../../components/Title/Title';
+import Image from '../../components/Image/Image';
 
 const cx = classNames.bind(styles)
 
@@ -15,6 +17,66 @@ const Home: React.FC<any> = () => {
                 <Header />
             </div>
             
+            <div className={cx('container')}>
+                <div className={cx('info')}>
+                    <div className={cx('intro')}>
+                        <h1>Hello 👋</h1>
+                    </div>
+
+                    <div className={cx('intro')}>
+                        <h1>I'm Thanh Dat Nguyen - tu3tle</h1>
+                    </div>
+
+                    <div className={cx('intro')}>
+                        <h1>front-end developer</h1>
+                    </div>
+
+                    <div className={cx('intro')}>
+                        <h1>based in Ho Chi Minh City, Vietnam</h1>
+                    </div>
+                </div>
+                
+                <div className={cx('contact')}>
+                    <div className={cx('social')}>
+                        <h3>Github</h3>
+                    </div>
+                    
+                    <div className={cx('social')}>
+                        <h3>Linkedln</h3>
+                    </div>
+
+                    <div className={cx('social')}>
+                        <h3>Facebook</h3>
+                    </div>
+
+                    <div className={cx('social')}>
+                        <h3>Blog</h3>
+                    </div>
+
+                    <div className={cx('social')}>
+                        <h3>Email</h3>
+                    </div>
+                </div>
+
+                <Title content='Work - My products' id='work'/>
+                <div className={cx('work')}>
+                    
+                    <div className={cx('components')}>
+                        <Image src='https://i.imgur.com/JfkXoG5.png'/>
+                        <h3>Lotteria Clone</h3>
+                    </div>
+
+                    <div className={cx('components')}>
+                        <Image src='https://i.imgur.com/BGwKMW0.png'/>
+                        <h3>tu3tle Porfolio</h3>
+                    </div>
+
+                    <div className={cx('components')}>
+                        <Image src='https://i.imgur.com/J8PjWiM.png'/>
+                        <h3>In js we trust - quiz app</h3>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
